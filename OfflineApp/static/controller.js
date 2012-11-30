0c78e29bce810e95656904040d5dcba1a0676b89
Claire Reynaud <claire.reynaud@epyx.ch>
2012-11-30 16:45:43 +0100

    Use html5 push and pop to have back working
    * Also started to fill the detail station view
    * Also tried to play with animations (not working, commented)

--- OfflineApp/static/controller.js
+++ OfflineApp/static/controller.js
@@ -1,12 +1,41 @@
+/* TODO what about localization in templates? */
+/* TODO templates with completion in the IDE? */
+/* TODO keep user position in stations list when back is pressed:
+   push the info to history and use this in showStations?
+ */
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function startController() {
+    // Revert to a previously saved state
+    window.addEventListener('popstate', function(event) {
+        dispatchToView();
+    });
+}
+
+function dispatchToView() {
+    var page = getParameterByName("page");
+    if (page === 'stations') {
+        showStations();
+    } else if (page === 'detail') {
+        var stationId = getParameterByName("stationId");
+        var stationName = getParameterByName("stationName");
+        showStationDetail(stationName, stationId);
+    } else {
+        showStations();
+    }
+}
+
 function showStations() {
+    window.history.pushState(null, null, "?page=stations");
     var json = $.getJSON('/static/stations.json');
     var template = $.get('/static/stations.tpl');
     $.when(json, template).done(function () {
-        var t = template.responseText;
-        var j = json.responseText;
-        console.log(t);
-        console.log(j);
-        var html = Mustache.to_html(t, $.parseJSON(j));
+        var stations = $.parseJSON(json.responseText);
+        var html = Mustache.to_html(template.responseText, stations);
         $('#stations').show();
         $('#stations').html(html);
         $('#stationDetail').hide();
@@ -15,15 +44,13 @@ function showStations() {
 }
 
 function showStationDetail(name, id) {
-    // TODO regarder l'extension hash de deck.js pour voir comment reloader en fonction du pound
+    window.history.pushState(null, null, "?page=detail&stationId=" + id + "&stationName=" + name);
     var json = $.getJSON('/static/station.json');
     var template = $.get('/static/station.tpl');
     $.when(json, template).done(function () {
-        var t = template.responseText;
-        var j = json.responseText;
-        console.log(t);
-        console.log(j);
-        var html = Mustache.to_html(t, $.parseJSON(j));
+        var station = $.parseJSON(json.responseText);
+        station.stationName = name;
+        var html = Mustache.to_html(template.responseText, station);
         $('#stations').hide();
         $('#stationDetail').show();
         $('#stationDetail').html(html);
--- OfflineApp/static/station.tpl
+++ OfflineApp/static/station.tpl
@@ -1,1 +1,35 @@
-{{stationId}}
+<div class="stationDetail">
+    <div class="stationDetailHeader">
+        <div class="stationStatus" style="color: {{status}}">
+            o
+        </div>
+        <div class="stationDetailName">
+            {{stationName}}
+        </div>
+    </div>
+    <div class="stationWind">
+        <div class="stationWindCurrent">
+            <div class="stationWindCurrentTitle">
+                Current wind:
+            </div>
+            Max: {{windMax}}<br/>
+            Average: {{windAverage}}<br/>
+            Trend: {{windTrend}}<br/>
+        </div>
+        <div class="stationWindHistory">
+            <div class="stationWindHistoryTitle">
+                History wind:
+            </div>
+            Min: {{windHistoryMin}}<br/>
+            Max: {{windHistoryMax}}<br/>
+            Average: {{windHistoryAverage}}<br/>
+        </div>
+    </div>
+    <div class="stationAir">
+        <div class="stationAirTitle">
+            Air:
+        </div>
+        Temperature: {{airTemperature}}<br/>
+        Humidity: {{airHumidity}}<br/>
+    </div>
+</div>
